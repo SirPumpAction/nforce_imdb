ca7d939ffdddf9260a7038ddf376a9bc415e0772
SirPumpAction <banana4uall@gmail.com>
2015-10-08 00:12:25 +0200

    Update nforce_imdb.user.js
    This is actually a proper minor version jump

--- nforce_imdb.user.js
+++ nforce_imdb.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NFOrce IMDB
 // @namespace    http://www.nfohump.com/
-// @version      1.0.4
+// @version      1.1.0
 // @description  Show inline IMDB.com ratings and movie details
 // @author       https://github.com/SirPumpAction
 // @match        http://*.nfohump.com/forum/*
